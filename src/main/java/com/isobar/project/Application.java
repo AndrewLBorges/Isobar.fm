7e8db113e567ada2a9510e280f62b8e9f52660e4
Andrew Borges <andrew.borges@esx.com.br>
2022-07-16 17:15:15 -0300

    final comment about the project

--- src/main/java/com/isobar/project/Application.java
+++ src/main/java/com/isobar/project/Application.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Application.class, args);
 	}
+
+
+	//The project is not quite working but I was able to organize it the way I intended. From here is discovering about the error I'm getting (goal failed to execute)
+	//and creating the other controllers/exceptions/advices. Also implementing the caching part wich I I'm still going to try later
 
 }
